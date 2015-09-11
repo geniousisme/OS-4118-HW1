7800c5a97db1fedd9214d6f94085cae74a823278
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-10 23:49:48 -0400

    finish readline & main structure of the shell file

--- shell/shell.c
+++ shell/shell.c
@@ -13,17 +13,116 @@ int cmd_exit(char **args);
 
 
 char *builtin_str[] = {
-  "cd",
-  "pwd",
-  "path",
-  "history",
-  "exit"
+     "cd",
+     "pwd",
+     "path",
+     "history",
+     "exit"
 };
 
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
+int (*builtin_func[])(char **) = {
+     &cmd_cd,
+     &cmd_pwd,
+     &cmd_path,
+     &cmd_history,
+     &cmd_exit
+};
+
+int builtin_str_leng(void) {
+    return sizeof(builtin_str) / sizeof(char *);
+};
+
+#define MAX_BUFF_SIZE 1024;
+
+int cmd_cd(char **args) {
+    return 1;
+};
+
+int cmd_pwd(char **args) {
+    return 1;
+};
+
+int cmd_path(char **args) {
+    return 1;
+};
+
+int cmd_history(char **args) {
+    return 1;
+};
+
+int cmd_exit(char **args) {
+    return 1;
+};
+
+char *cmd_readline(void) {
+     int buffer_size = MAX_BUFF_SIZE;
+     int pos         = 0;
+     char* buffer    = malloc(buffer_size * sizeof(char));
+     char c;
+     while(1) {
+           c = getchar();
+           if (c == EOF || c == '\n') {
+               buffer[pos] = '\0';
+               // printf("test read line: %s \n", buffer);
+               return buffer;
+           }
+           else {
+               buffer[pos] = c;
+           };
+           pos++;
+           // if exceed the max buffer size, realloc
+           if (pos >= buffer_size) {
+               buffer_size += MAX_BUFF_SIZE;
+               buffer = realloc(buffer, buffer_size);
+               if (!buffer) {
+                   fprintf(stderr, "allocation error!!");
+                   exit(EXIT_FAILURE);
+               };
+           };
+     };
+};
+
+char **cmd_parse(char *line) {
+     // printf("hey, here is parse!!!!\n");
+     // char **tmp = {&line};
+     return ;
+};
+
+int cmd_launch(char **args) {
+    return 1;
+};
+
+
+int cmd_execute(char **args) {
+    int i;
+    
+    if (args[0] == NULL) {
+        // empty command input, skip and continue
+        return 1;
+    };
+
+    for (i = 0; i < builtin_str_leng(); i++) {
+         if (strcmp(args[0], builtin_str[i]) == 0) {
+             return (*builtin_func[i])(args);
+         };
+    };
+
+    return cmd_launch(args);
+}; 
+
+
+void cmd_loop(void) {
+     char *line;
+     char **args;
+     int  status = 1;
+     while(status) {
+           printf("$ ");
+           line   = cmd_readline();
+           args   = cmd_parse(line);
+           status = cmd_execute(args);
+           free(line);
+           free(args);
+     };
 };
 
 int main (int argc, char **argv) {
