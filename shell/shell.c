ac779915bde0aac22eb87b5d2848e50649b8a38a
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-10 20:50:31 -0400

    start shell.c

--- shell/shell.c
+++ shell/shell.c
@@ -1,5 +1,32 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
-int main(int argc, char **argv) {
-    printf ("lalala\n");
-	return 0;
-}
+#include <string.h>
+
+
+int cmd_cd(char **args);
+int cmd_pwd(char **args);
+int cmd_path(char **args);
+int cmd_history(char **args);
+int cmd_exit(char **args);
+
+
+char *builtin_str[] = {
+  "cd",
+  "pwd",
+  "path",
+  "history",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int main (int argc, char **argv) {
+    cmd_loop();
+    return EXIT_SUCCESS;
+};
