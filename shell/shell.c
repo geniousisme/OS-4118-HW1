41b1f10752be9a35bd54227a9b096f123bebbf97
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-17 18:00:58 -0400

    finish style checking

--- shell/shell.c
+++ shell/shell.c
@@ -4,21 +4,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-
+#include "shell.h"
 
 #define MAX_HIST_SIZE     100
 #define MAX_BUFF_SIZE     1024
 #define MAX_TOK_BUFF_SIZE 64
 #define DEFAULT_PATH      ""
 #define TOKEN_DELIM       " \t\n\r"
-#define PATH_DELIM  	  ":"
+#define PATH_DELIM        ":"
 #define PATH_ENV          getenv("PATH")
-
-int cmd_cd(char **args);
-int cmd_pwd(char **args);
-int cmd_path(char **args);
-int cmd_history(char **args);
-int cmd_exit(char **args);
 
 char *origin_path_env;
 char *history[MAX_HIST_SIZE + 1];
