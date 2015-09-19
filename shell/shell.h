b2de0d142a2a8b09d85a1eb32552152e91a89f1f
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-19 01:47:31 -0400

    try to compare difference

--- shell/shell.h
+++ shell/shell.h
@@ -1,5 +1,15 @@
-int cmd_cd(char **args);
-int cmd_pwd(char **args);
-int cmd_path(char **args);
-int cmd_history(char **args);
-int cmd_exit(char **args);
+#define MAX_HIST_SIZE     100
+#define MAX_BUFF_SIZE     1024
+#define MAX_TOK_BUFF_SIZE 64
+#define DEFAULT_PATH      ""
+#define TOKEN_DELIM       " \t\n\r"
+#define PATH_DELIM        ":"
+#define PATH_ENV          getenv("PATH")
+
+// int cmd_cd(char **args);
+// int cmd_pwd(char **args);
+// int cmd_path(char **args);
+// // int cmd_history(char **args);
+// int cmd_exit(char **args);
+
+
