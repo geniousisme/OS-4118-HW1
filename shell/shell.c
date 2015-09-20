083f21d4ba96ce536bd0ee4c5c45bce588b39324
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-20 15:09:24 -0400

    push to remote first

--- shell/shell.c
+++ shell/shell.c
@@ -79,35 +79,35 @@ void change_path_env(char *new_path)
 
 void add_history(char *line)
 {
-	// char *line_copy = malloc(sizeof(char) * (strlen(line) + 1));
-	// char *cmd       = malloc(sizeof(char) * (strlen(line) + 1));
+	char *line_copy = malloc(sizeof(char) * (strlen(line) + 1));
+	char *cmd       = malloc(sizeof(char) * (strlen(line) + 1));
 
-	// strcpy(line_copy, line);
-	// strcpy(cmd, line);
-	// cmd = strtok(cmd, TOKEN_DELIM);
-	// if (cmd == NULL || strcmp(cmd, "history") == 0 || strcmp(cmd, "exit") == 0) {
-	// 	free(cmd);
- //        free(line_copy);
-	// 	return;
-	// };
+	strcpy(line_copy, line);
+	strcpy(cmd, line);
+	cmd = strtok(cmd, TOKEN_DELIM);
+	if (cmd == NULL || strcmp(cmd, "history") == 0 || strcmp(cmd, "exit") == 0) {
+		free(cmd);
+        free(line_copy);
+		return;
+	};
 
-	// int pos = 0;
+	int pos = 0;
 
-	// while (history[pos] != NULL)
-	// 	pos++;
+	while (history[pos] != NULL)
+		pos++;
 
-	// history[pos] = line_copy;
+	history[pos] = line_copy;
 
-	// /* update the history list  */
-	// if (pos >= MAX_HIST_SIZE) {
-	// 	int i;
+	/* update the history list  */
+	if (pos >= MAX_HIST_SIZE) {
+		int i;
 
-	// 	for (i = 1; i < MAX_HIST_SIZE + 1; i++)
-	// 		history[i - 1] = history[i];
-	// 	history[i - 1] = NULL;
-	// };
-    // free(line_copy);
-	// free(cmd);
+		for (i = 1; i < MAX_HIST_SIZE + 1; i++)
+			history[i - 1] = history[i];
+		history[i - 1] = NULL;
+	};
+    free(line_copy);
+	free(cmd);
 	return;
 };
 
